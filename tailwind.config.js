70a12a1f2e12cdb32902a6f9a32309ac6c870af5
Nitin Khare <kharenitin777@gmail.com>
2025-04-16 17:10:02 +0530

    updated about section and added font awesome library for icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./index.html",        // root HTML file
-    "./src/**/*.{html,js}" // also include any future html/js files inside src
+    "./src/**/*.{html,js}" // includes any future html/js files inside src
   ],
   theme: {
     extend: {},
