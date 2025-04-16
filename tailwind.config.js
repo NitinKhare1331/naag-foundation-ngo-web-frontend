8fe69def9fe35f6aba9d406fcac007c56108bfb0
Nitin Khare <kharenitin777@gmail.com>
2025-04-16 10:33:46 +0530

    Added custom tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",        // root HTML file
+    "./src/**/*.{html,js}" // also include any future html/js files inside src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
